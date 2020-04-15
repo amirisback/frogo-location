62e3ace8c7f847cf1adc865667c56cfc0b3e4e4a
amirisback <faisalamircs@gmail.com>
2020-04-15 15:52:55 +0700

    Initial commit

---
+++ app/src/main/java/com/frogobox/frogolocation/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.frogobox.frogolocation
+
+import android.location.Location
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.OnSuccessListener
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
