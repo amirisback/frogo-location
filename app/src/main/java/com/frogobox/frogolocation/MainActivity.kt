f026ec0b56eefb27f461dabe40805067a1be2927
amirisback <faisalamircs@gmail.com>
2021-02-24 11:32:38 +0700

    update: build.gradle

--- app/src/main/java/com/frogobox/frogolocation/MainActivity.kt
+++ app/src/main/java/com/frogobox/frogolocation/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.frogobox.frogolocation
 
-import android.location.Location
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.OnSuccessListener
 
 class MainActivity : AppCompatActivity() {
 
